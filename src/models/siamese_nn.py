a605c01c56139d4dfcb5ba2e3e8fbaac9e0611d6
KPajak409 <pajak.karol414@gmail.com>
2023-10-29 20:08:32 +0100

    Project files structure

---
+++ src/models/siamese_nn.py
@@ -0,0 +1,13 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Siamese_nn(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self,x):
+        return x
+    
+
+if __name__ == '__main__':
+    print('hello')
