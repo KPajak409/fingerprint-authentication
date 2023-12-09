5bf134b061b15e1e3a3adc0330d96c79da08f68d
KPajak409 <pajak.karol414@gmail.com>
2023-12-09 02:53:41 +0100

    Custom dataset extraction to file, adjusting to run on gpu, added model results testing code

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,69 +1,36 @@
 #%%
-import torch
+import torch, math
 import torch.optim as optim
 import torch.nn as nn
 import torch.nn.functional as F
-from pathlib import Path
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import DataLoader
 from torchvision import transforms
-from PIL import Image
 from siamese_nn import Siamese_nn
-import os, random
+from src.data.fingerprint_dataset import SiameseDataset
 
-current_dir = Path(__file__)
-project_dir = [p for p in current_dir.parents if p.parts[-1]=='fingerprint-authentication'][0]
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print(device)
+# hyperparams
+batch_size =1
 
-out_path = f'{project_dir}\\data\\final\\'
 
 def train(model, dataloader, criterion, optimizer, num_epochs=10):
     model.train()
+    steps = math.floor(len(dataloader)/batch_size)
     for epoch in range(num_epochs):
-        for img1, img2, label in dataloader:
+        for i, (img1, img2, label) in enumerate(dataloader):
             optimizer.zero_grad()
+            img1 = img1.to(device)
+            img2 = img2.to(device)
+            label = label.to(device)
             output1, output2 = model(img1, img2)
             loss = criterion(output1, output2, label)
             loss.backward()
             optimizer.step()
-
-        print(f'Epoch [{epoch + 1}/{num_epochs}], Loss: {loss.item():.4f}')
             
-class SiameseDataset(Dataset):
-    def __init__(self, image_folder, transform=None):
-        self.image_folder = image_folder
-        self.transform = transform
-        self.data = []
-        
-        file_list = os.listdir(image_folder)
-        usersIndexes = []
-        for x in file_list:
-          if x[0:2] not in usersIndexes:
-            usersIndexes.append(x[0:2])
+            if i%10==0:
+                print(f'Epoch [{epoch + 1}/{num_epochs}], step: [{i}/{steps}] Loss: {loss.item():.4f}')
             
-        for user in usersIndexes:
-          usersImages = [x for x in file_list if str(user) in x]
-          notUsersImages = [x for x in file_list if str(user) not in x]
-          for userImage in usersImages:
-            for userImageCopy in usersImages:
-              self.data.append([userImage, userImageCopy, 1.0])
-          for userImage in usersImages:
-            for _ in usersImages:
-              self.data.append([userImage, notUsersImages[random.randrange(len(notUsersImages))], 0.0])
-        
-    def __len__(self):
-        return len(self.image_folder)
-
-    def __getitem__(self, index):
-        img1 = Image.open(out_path + '\\' + self.data[index][0])
-        img2 = Image.open(out_path + '\\' + self.data[index][1])  
-
-        if self.transform:
-            img1 = self.transform(img1)
-            img2 = self.transform(img2)
-            
-        if (self.data[index][2] == 0.0):
-          return img1, img2, torch.Tensor([0.0]) 
-        elif (self.data[index][2] == 1.0):
-          return img1, img2, torch.Tensor([1.0])
 
 class ContrastiveLoss(nn.Module):
     def __init__(self, margin=2.0):
@@ -79,15 +46,58 @@ class ContrastiveLoss(nn.Module):
 
 transform = transforms.Compose([transforms.ToTensor()])
 
-siameseDataset = SiameseDataset(image_folder=out_path, transform=transform)
-dataloader = DataLoader(siameseDataset, shuffle=True, batch_size=200)
+siameseDataset = SiameseDataset(transform=transform, device=device)
+dataloader = DataLoader(siameseDataset, shuffle=True, batch_size=batch_size)
 
-
-siameseNet = Siamese_nn()
+siameseNet = Siamese_nn().to(device)
 criterion = ContrastiveLoss()
 optimizer = optim.Adam(siameseNet.parameters(), lr=0.001)
 
+#%%
 
-train(siameseNet, dataloader, criterion, optimizer, num_epochs=20)
+train(siameseNet, dataloader, criterion, optimizer, num_epochs=1)
 
 # %%
+# Testing results
+import matplotlib.pyplot as plt
+
+with torch.no_grad():
+    test_dataloader = DataLoader(siameseDataset, shuffle=True, batch_size=9)
+    test_dataloader_iter = iter(test_dataloader)
+    images1, images2, label = next(test_dataloader_iter)
+
+    siameseNet = siameseNet.to('cpu')
+    out1, out2 = siameseNet(images1, images2)
+    # problem with contrastive loss
+    print(out1.shape, out2.shape)
+    pred = criterion.forward(out1[0], out2[0], label[0])
+    pred2 = criterion.forward(out1, out2, label)
+    
+    print(pred, pred2)
+    
+    images1 = torch.permute(images1, (0,2,3,1))
+    images2 = torch.permute(images2, (0,2,3,1))
+    
+    plt.rcParams.update({'font.size': 6})
+    for i in range(images1.shape[0]):
+        pair = torch.hstack((images1[i], images2[i]))
+        pred = criterion.forward(out1[0], out2[0], label[0])
+        plt.subplot(3,3,i+1)
+        
+        plt.xticks([])
+        plt.yticks([])
+        plt.title(f'{label[i].item()} CL: {round(pred.item(),5)}')
+        plt.tight_layout()
+        plt.imshow(pair)
+    
+
+# %%
+
+class_balance = torch.tensor((0,0))
+for i, (img1, img2, label) in enumerate(dataloader):
+    if label == 0:
+        class_balance[0] += 1
+    elif label == 1:
+        class_balance[1] += 1
+
+print(class_balance, len(siameseDataset))
